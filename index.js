801c0ff65a8ab5a6a5dfe5043ac921b0a81b85b2
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2024-02-05 00:48:40 +0300

    feat: implement up command

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const readline = require('readline');
+const readline = require("readline");
+const path = require("path");
+const os = require("os");
 
 function getCommandLineArg(argName) {
     const args = process.argv.slice(2);
     let argValue;
 
-    for(let i = 0; i < args.length; i++) {
-        if(args[i].startsWith(`--${argName}=`)) {
-            argValue = args[i].split('=')[1];
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith(`--${argName}=`)) {
+            argValue = args[i].split("=")[1];
             break;
         }
     }
@@ -14,5 +16,49 @@ function getCommandLineArg(argName) {
     return argValue;
 }
 
-const username = getCommandLineArg('username');
+function up(){
+    const rootDirectoryPath = path.parse(os.homedir()).root;
+    if (currentDirectoryPath !== rootDirectoryPath) {
+        const parentDirectoryPath = path.dirname(currentDirectoryPath);
+        currentDirectoryPath = parentDirectoryPath;
+    } else {
+        console.log("Already at the root folder");
+    }
+}
+
+const username = getCommandLineArg("username");
 console.log(`Welcome to the File Manager, ${username}!`);
+let currentDirectoryPath = path.join(process.env.SystemDrive, process.env.HOMEPATH);
+console.log(`You are currently in ${currentDirectoryPath}`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> "
+});
+
+rl.prompt();
+
+rl.on("line", (line) => {
+    if (line === "up") {
+        up();
+    }
+
+    rl.prompt();
+
+    if (line === ".exit") {
+        rl.close();
+    }
+    // } else if (line === ".help") {
+    //     console.log(`Received command: ${line}`);
+    // } else {
+    //     console.log("Invalid input");
+    // }
+    
+    console.log(`You are currently in ${currentDirectoryPath}`);
+});
+
+rl.on("close", () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit(0);
+});
