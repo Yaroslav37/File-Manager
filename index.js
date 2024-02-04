73fbb3527405980d16cade1991231028d2dd814b
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2024-02-05 01:12:17 +0300

    feat: implment cd command

--- index.js
+++ index.js
@@ -1,64 +1,89 @@
-const readline = require("readline");
-const path = require("path");
-const os = require("os");
+import { createInterface } from "readline";
+import { parse, dirname, isAbsolute, join } from "path";
+import { homedir } from "os";
+import { existsSync } from "fs";
 
 function getCommandLineArg(argName) {
-    const args = process.argv.slice(2);
-    let argValue;
+  const args = process.argv.slice(2);
+  let argValue;
 
-    for (let i = 0; i < args.length; i++) {
-        if (args[i].startsWith(`--${argName}=`)) {
-            argValue = args[i].split("=")[1];
-            break;
-        }
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].startsWith(`--${argName}=`)) {
+      argValue = args[i].split("=")[1];
+      break;
     }
+  }
 
-    return argValue;
+  return argValue;
 }
 
-function up(){
-    const rootDirectoryPath = path.parse(os.homedir()).root;
-    if (currentDirectoryPath !== rootDirectoryPath) {
-        const parentDirectoryPath = path.dirname(currentDirectoryPath);
-        currentDirectoryPath = parentDirectoryPath;
-    } else {
-        console.log("Already at the root folder");
-    }
+function up() {
+  const rootDirectoryPath = parse(homedir()).root;
+  if (currentDirectoryPath !== rootDirectoryPath) {
+    const parentDirectoryPath = dirname(currentDirectoryPath);
+    currentDirectoryPath = parentDirectoryPath;
+  } else {
+    console.log("Already at the root folder");
+  }
+}
+
+function cd(targetPath) {
+  let newDirectoryPath;
+
+  if (isAbsolute(targetPath)) {
+    newDirectoryPath = targetPath;
+  } else {
+    newDirectoryPath = join(currentDirectoryPath, targetPath);
+  }
+
+  if (existsSync(newDirectoryPath)) {
+    currentDirectoryPath = newDirectoryPath;
+  } else {
+    console.log(`Operation failed: The system cannot find the path specified.`);
+  }
 }
 
 const username = getCommandLineArg("username");
 console.log(`Welcome to the File Manager, ${username}!`);
-let currentDirectoryPath = path.join(process.env.SystemDrive, process.env.HOMEPATH);
+let currentDirectoryPath = join(
+  process.env.SystemDrive,
+  process.env.HOMEPATH
+);
 console.log(`You are currently in ${currentDirectoryPath}`);
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "> "
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
 });
 
 rl.prompt();
 
 rl.on("line", (line) => {
-    if (line === "up") {
-        up();
-    }
+  if (line === "up") {
+    up();
+  }
 
-    rl.prompt();
+  if (line.startsWith("cd ")) {
+    const targetPath = line.split(" ")[1];
+    cd(targetPath);
+  }
 
-    if (line === ".exit") {
-        rl.close();
-    }
-    // } else if (line === ".help") {
-    //     console.log(`Received command: ${line}`);
-    // } else {
-    //     console.log("Invalid input");
-    // }
-    
-    console.log(`You are currently in ${currentDirectoryPath}`);
+  rl.prompt();
+
+  if (line === ".exit") {
+    rl.close();
+  }
+  // } else if (line === ".help") {
+  //     console.log(`Received command: ${line}`);
+  // } else {
+  //     console.log("Invalid input");
+  // }
+
+  console.log(`You are currently in ${currentDirectoryPath}`);
 });
 
 rl.on("close", () => {
-    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-    process.exit(0);
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit(0);
 });
